825a227b350a3fa78efeffcbbaf201e7b7a94086
SharanRaj <sharan9678@gmail.com>
2021-11-05 12:56:15 +0530

    completed challenge

--- server.js
+++ server.js
@@ -3,13 +3,17 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const morgan = require("morgan");
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+//morgan logger middleware
+app.use(morgan('dev'));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,15 +23,23 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", (req, res) => {
+  let ipaddress = req.ip;
+  let language = req.get('accept-language');
+  let software = req.get('user-agent');
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
